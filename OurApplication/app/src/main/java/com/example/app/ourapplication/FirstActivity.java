98ac9832a3f712af79f9612659a9b8fee17a14a5
--global <siva.a.shankar@gmail.com>
2016-02-26 20:25:39 +0530

    Added Support for a POST query and few debugging logs.

--- OurApplication/app/src/main/java/com/example/app/ourapplication/FirstActivity.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/FirstActivity.java
@@ -2,22 +2,25 @@ package com.example.app.ourapplication;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Starting point for our App, lets start our trials here.
@@ -25,31 +28,56 @@ import javax.net.ssl.HttpsURLConnection;
 public class FirstActivity extends AppCompatActivity {
 
     private final String TAG = FirstActivity.class.getSimpleName();
+    private final String ACCESS_URL = "http://ec2-54-254-185-153.ap-southeast-1.compute.amazonaws.com";
     private final int CONNECTION_TIMEOUT = 60000;
+
+    // We need to define the Rest Api url here to establish the connection.
+//    private String mAccessURL="http://192.168.1.10:8889";
 
     private EditText mUserNameBox;
     private EditText mPasswordBox;
     private Button mLoginButton;
-
+    private TextView mLogText;
+    private ViewFlipper mFlipper;
+    private EditText mUrlBox;
     private ProgressDialog mLoginProgressDlg;
+
+    private String mReadMessage;
+    private String mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        mUserNameBox = (EditText) findViewById(R.id.editText);
-        mPasswordBox = (EditText) findViewById(R.id.editText2);
+        mUserNameBox = (EditText) findViewById(R.id.username_field);
+        mPasswordBox = (EditText) findViewById(R.id.password_field);
         mLoginButton = (Button) findViewById(R.id.login_button);
+        mLogText = (TextView) findViewById(R.id.log_text);
+        mFlipper = (ViewFlipper) findViewById(R.id.flipper);
+        mUrlBox = (EditText) findViewById(R.id.url_field);
+        mUrlBox.setText(ACCESS_URL);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUserNameBox.getText().toString();
-                String password = mPasswordBox.getText().toString();
-                new LoginTask().execute(username,password);
+                String body = getRequestBody();
+                String connectingUrl = mUrlBox.getText().toString();
+                if(TextUtils.isEmpty(connectingUrl)){
+                    connectingUrl = ACCESS_URL;
+                }
+                new LoginTask().execute(body,connectingUrl);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mFlipper.getDisplayedChild() != 0){
+            flipToPage(0);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     private void showProgressDialog() {
@@ -62,44 +90,9 @@ public class FirstActivity extends AppCompatActivity {
         mLoginProgressDlg.show();
     }
 
-    private class LoginTask extends AsyncTask<String,Void,Integer>{
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showProgressDialog();
-        }
-
-        @Override
-        protected Integer doInBackground(String... params) {
-            // The web services part to be plugged here.
-            String username = params[0];
-            String password = params[1];
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            int result = 0;
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer response) {
-            super.onPostExecute(response);
-            mLoginProgressDlg.dismiss();
-            Log.d(TAG,"Response : "+response);
-        }
-    }
-
-    private void sampleWebServices(String sReqBody,String sAccessURL) throws IOException {
-        // We need to define the Rest Api url here to establish the connection.
-        String accessURL="https://ggogle.com/oauth2/token";
-
+    private void sampleWebServices(String sReqBody,String url) throws IOException {
         // We opened the connection using the URL.
-        HttpURLConnection conn = (HttpURLConnection) new URL(accessURL).openConnection();
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
 
         // These are basic APIs we need to use.
         conn.setRequestMethod("POST");
@@ -120,8 +113,8 @@ public class FirstActivity extends AppCompatActivity {
 
         // Tis is read from the connection.
         if (HttpResult == HttpURLConnection.HTTP_OK) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
-            String line = null;
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line + "\n");
             }
@@ -129,12 +122,75 @@ public class FirstActivity extends AppCompatActivity {
 
             if (conn != null)
                 conn.disconnect();
+            mReadMessage = mReadMessage+sb.toString();
         }else {
             Log.e(TAG, msg);
             if (conn != null)
                 conn.disconnect();
-
             throw new IOException(msg);
+        }
+    }
+
+    private String getRequestBody(){
+        JSONObject body = new JSONObject();
+        try {
+            body.put("name",mUserNameBox.getText());
+            body.put("password",mPasswordBox.getText());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return body.toString();
+    }
+
+    private void flipToPage(int page){
+        switch (page){
+            case 0:
+                break;
+            case 1:
+                break;
+        }
+        mFlipper.setDisplayedChild(page);
+    }
+
+    private class LoginTask extends AsyncTask<String,Void,Boolean>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mReadMessage = "Reading :\n\n";
+            mErrorMessage = "";
+
+            showProgressDialog();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            boolean result = false;
+            // The web services part to be plugged here.
+            String body = params[0];
+            String url = params[1];
+
+            try {
+                sampleWebServices(body,url);
+                result = true;
+            } catch (IOException e) {
+                mReadMessage = mReadMessage+" Failed.";
+                mErrorMessage = "Error Message is: \n\n";
+                mErrorMessage = mErrorMessage+e.toString();
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean response) {
+            super.onPostExecute(response);
+            mLoginProgressDlg.dismiss();
+            Log.d(TAG, "Response : "+response);
+            mLogText.setText(mReadMessage);
+            mLogText.append("\n\n"+mErrorMessage);
+            flipToPage(1);
         }
     }
 }
