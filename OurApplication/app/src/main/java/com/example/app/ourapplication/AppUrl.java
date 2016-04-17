60dd61ca4df76bd4037263cfefc172c8cd92d766
--global <siva.a.shankar@gmail.com>
2016-04-17 16:54:43 +0530

    Socket url update

--- OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
@@ -5,5 +5,5 @@ package com.example.app.ourapplication;
  */
 public class AppUrl {
     public static final String SERVER_URL = "http://ec2-54-254-185-153.ap-southeast-1.compute.amazonaws.com";
-    public static final String WS_URL = "ws://ec2-54-254-185-153.ap-southeast-1.compute.amazonaws.com:8080";
+    public static final String WS_URL = "ws://ec2-52-77-242-215.ap-southeast-1.compute.amazonaws.com:8080";
 }
