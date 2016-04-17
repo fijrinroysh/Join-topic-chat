854d909102bb38f35a4df126cc8bc071825916bb
--global <siva.a.shankar@gmail.com>
2016-04-17 16:38:58 +0530

    Cleaned up the code with UI flow.

---
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
@@ -0,0 +1,9 @@
+package com.example.app.ourapplication;
+
+/**
+ * Created by sarumugam on 17/04/16.
+ */
+public class AppUrl {
+    public static final String SERVER_URL = "http://ec2-54-254-185-153.ap-southeast-1.compute.amazonaws.com";
+    public static final String WS_URL = "ws://ec2-54-254-185-153.ap-southeast-1.compute.amazonaws.com:8080";
+}
