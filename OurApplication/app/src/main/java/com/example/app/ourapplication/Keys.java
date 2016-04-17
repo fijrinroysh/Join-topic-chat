b69d92a90f24958fc8a8b6bb5458d23e882550ca
--global <siva.a.shankar@gmail.com>
2016-04-17 18:27:16 +0530

    Few more message handling.

---
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/Keys.java
@@ -0,0 +1,10 @@
+package com.example.app.ourapplication;
+
+/**
+ * Created by sarumugam on 17/04/16.
+ */
+public class Keys {
+    public static final String KEY_TOKEN = "token";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_NAME = "name";
+}
