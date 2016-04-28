06ba5a249d933de431f8c46f696da86db47f63b1
--global <siva.a.shankar@gmail.com>
2016-04-28 23:22:18 +0530

    SignUp url update.

--- OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
@@ -4,6 +4,7 @@ package com.example.app.ourapplication;
  * Created by sarumugam on 17/04/16.
  */
 public class AppUrl {
-    public static final String SERVER_URL = "http://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com/signin";
+    public static final String LOGIN_URL = "http://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com/signin";
+    public static final String SIGN_UP_URL = "http://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com/signup";
     public static final String WS_URL = "ws://ec2-52-77-242-215.ap-southeast-1.compute.amazonaws.com:8080";
 }
