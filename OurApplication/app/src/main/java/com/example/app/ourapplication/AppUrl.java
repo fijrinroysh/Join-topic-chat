2c8ae025428e3d0be03d622d53fd02792f681fb3
--global <siva.a.shankar@gmail.com>
2016-06-21 21:33:04 +0530

    Preference related change.

--- OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/AppUrl.java
@@ -7,4 +7,5 @@ public class AppUrl {
     public static final String LOGIN_URL = "http://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com/signin";
     public static final String SIGN_UP_URL = "http://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com/signup";
     public static final String WS_URL = "ws://ec2-52-77-242-215.ap-southeast-1.compute.amazonaws.com:8080";
+    public static final String WS_TEST_URL = "ws://ec2-52-77-255-151.ap-southeast-1.compute.amazonaws.com:8080";
 }
---
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/pref/PreferenceEditor.java
@@ -0,0 +1,35 @@
+package com.example.app.ourapplication.pref;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by sarumugam on 21/06/16.
+ */
+public class PreferenceEditor {
+
+    private static PreferenceEditor mEditor;
+    private SharedPreferences mSharedPrefs;
+
+    public static PreferenceEditor getInstance(Context context){
+        if(mEditor == null){
+            mEditor = new PreferenceEditor(context);
+
+        }
+        return mEditor;
+    }
+
+    private PreferenceEditor(Context context){
+        mSharedPrefs = context.getSharedPreferences(PrefKeys.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public String getLoggedInUserName() {
+        return mSharedPrefs.getString(PrefKeys.LOGGED_IN_USER_NAME, null);
+    }
+
+    public void setLoggedInUserName(String loginId) {
+        SharedPreferences.Editor editor = mSharedPrefs.edit();
+        editor.putString(PrefKeys.LOGGED_IN_USER_NAME, loginId);
+        editor.apply();
+    }
+}
