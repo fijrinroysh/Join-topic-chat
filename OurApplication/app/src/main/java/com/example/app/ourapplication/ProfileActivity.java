667daa7b4d224800ac77f08fb8307b51a00c6883
--global <siva.a.shankar@gmail.com>
2016-08-15 23:11:42 +0530

    Profile picture coropping.

--- OurApplication/app/src/main/java/com/example/app/ourapplication/ProfileActivity.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/ProfileActivity.java
@@ -1,11 +1,10 @@
 package com.example.app.ourapplication;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,14 +16,11 @@ import android.widget.ImageView;
 
 import com.example.app.ourapplication.pref.PreferenceEditor;
 import com.example.app.ourapplication.util.Helper;
-import com.example.app.ourapplication.util.UI;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Created by ROYSH on 8/8/2016.
@@ -34,10 +30,10 @@ public class ProfileActivity extends AppCompatActivity{
     private final String TAG = ProfileActivity.class.getSimpleName();
     public static Bitmap mBitmap;
     private String imageprofilestring;
-    ImageView profileImgView;
+    private ImageView profileImgView;
     public static final int UPDATE_PIC = 1;
     public DBHelper mDBHelper = new DBHelper(this);
-    String name = PreferenceEditor.getInstance(this).getLoggedInUserName();
+    private String name = PreferenceEditor.getInstance(this).getLoggedInUserName();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +48,6 @@ public class ProfileActivity extends AppCompatActivity{
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
                     showFileChooser();
                 }
             });
@@ -62,97 +56,96 @@ public class ProfileActivity extends AppCompatActivity{
 
 
     private void showFileChooser() {
-
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), UPDATE_PIC );
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 0);
+        intent.putExtra("aspectY", 0);
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 150);
+
+        try {
+            intent.putExtra("return-data", true);
+            startActivityForResult(Intent.createChooser(intent,"Complete action using"), UPDATE_PIC);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+            Snackbar.make(profileImgView, "Activity not found", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode){
             case UPDATE_PIC:
+                if (resultCode == RESULT_OK && data != null) {
 
-        if (resultCode == RESULT_OK && data != null
-                && data.getData() != null) {
-            Uri filePath = data.getData();
-            try {
-                //Getting the Bitmap from Gallery
-                mBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                profileImgView.setImageBitmap(mBitmap);
-                imageprofilestring = Helper.getStringImage(mBitmap);
-                Log.d(TAG, "Image message value length : " + imageprofilestring.length());
-                Log.d(TAG, "Image message value is : " + imageprofilestring);
+                    Bundle extras = data.getExtras();
+                    if (extras != null) {
+                        mBitmap = extras.getParcelable("data");
+                        if (mBitmap != null) {
+                            Log.d(TAG,"L : "+mBitmap.getWidth()+ "  : "+mBitmap.getScaledHeight(getResources().getDisplayMetrics()));
+                        }
+                        profileImgView.setImageBitmap(mBitmap);
+                    }
+                    imageprofilestring = Helper.getStringImage(mBitmap);
+                    Log.d(TAG, "Image message value length : " + imageprofilestring.length());
+                    Log.d(TAG, "Image message value is : " + imageprofilestring);
 
-                if(!TextUtils.isEmpty(name)) {
-                    String body = Helper.getUpdateProfileBody(name, Keys.KEY_PROFIMG, imageprofilestring);
-                    new ProfileUpdateTask().execute(body);
-                    mDBHelper.updateProfile(body);
+                    if(!TextUtils.isEmpty(name)) {
+                        String body = Helper.getUpdateProfileBody(name, Keys.KEY_PROFIMG, imageprofilestring);
+                        new ProfileUpdateTask().execute(body);
+                        mDBHelper.updateProfile(body);
+                    }
                 }
+                break;
+        }
+    }
 
+    private class ProfileUpdateTask extends AsyncTask<String,Void,String> {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // UI.showProgressDialog(HomeFeedActivity.this, getString(R.string.login_progress));
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String body = params[0];
+            String response = null;
+            try {
+                response = Helper.updateProfile(body);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-        }
-                break;
+            return response;
         }
 
-    }
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            Log.d(TAG, "Response : " + response);
+            if(response != null){
 
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean isSuccess = jsonObject.getBoolean(Keys.KEY_SUCCESS);
 
+                    if(isSuccess) {
 
+                        Log.d(TAG, "Profile information Updated");
 
-private class ProfileUpdateTask extends AsyncTask<String,Void,String> {
-
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-       // UI.showProgressDialog(HomeFeedActivity.this, getString(R.string.login_progress));
-    }
-
-    @Override
-    protected String doInBackground(String... params) {
-        String body = params[0];
-        String response = null;
-        try {
-            response = Helper.updateProfile(body);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response;
-    }
-
-    @Override
-    protected void onPostExecute(String response) {
-        super.onPostExecute(response);
-        Log.d(TAG, "Response : " + response);
-        if(response != null){
-
-            try {
-                JSONObject jsonObject = new JSONObject(response);
-                boolean isSuccess = jsonObject.getBoolean(Keys.KEY_SUCCESS);
-
-                if(isSuccess) {
-
-                    Log.d(TAG, "Profile information Updated");
-
-                }else{
-                  //  Snackbar.make(view, "Profile information Updated", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                    Log.d(TAG, "Profile information not Updated");
+                    }else{
+                        //  Snackbar.make(view, "Profile information Updated", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        Log.d(TAG, "Profile information not Updated");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
+            }else{
+                Log.d(TAG, "Update response is NULL");
             }
-        }else{
-            Log.d(TAG, "Update response is NULL");
         }
     }
-}
-
 }
