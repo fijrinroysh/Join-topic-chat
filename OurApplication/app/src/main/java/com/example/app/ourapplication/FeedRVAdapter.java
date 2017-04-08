3b8b344a13a0b2e50900d2fc64938644617cdb7d
Fijrin Roysh <Fijrin Roysh>
2017-04-08 14:15:40 +0530

    Image layout changes

--- OurApplication/app/src/main/java/com/example/app/ourapplication/FeedRVAdapter.java
+++ OurApplication/app/src/main/java/com/example/app/ourapplication/FeedRVAdapter.java
@@ -139,7 +139,7 @@ public class FeedRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             return new PersonViewHolder1(v);
         } else if (viewType == 2) {
             Log.d(TAG, "PersonViewHolder2 created");
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_item1, viewGroup, false);
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_item_image, viewGroup, false);
             return new PersonViewHolder2(v);
         } else if (viewType == 3) {
             Log.d(TAG, "PersonViewHolder3 created");
